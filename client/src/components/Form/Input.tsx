ab2aa633dc642d7d5163d46962d71c18421d837b
Jon Jackson <ocskier@gmail.com>
2019-10-26 12:05:47 -0400

    broken- working on SignUp and Form Input

--- client/src/components/Form/Input.tsx
+++ client/src/components/Form/Input.tsx
@@ -1,8 +1,13 @@
-import React, { ReactPropTypes, FC } from "react";
+import React, { ReactPropTypes, FC, ChangeEvent } from "react";
 
-interface InputProps extends ReactPropTypes {}
+interface InputProps extends ReactPropTypes {
+  type: string;
+  name: string;
+  value: string;
+  onChange: (event: ChangeEvent<{ name?: string; value: unknown }>) => void;
+}
 
-export const Input: FC<InputProps> = props => (
+export const Input: FC<InputProps & any> = props => (
   <div className="form-group">
     <input className="form-control" {...props} />
   </div>
--- client/src/pages/Auth/SignupForm.tsx
+++ client/src/pages/Auth/SignupForm.tsx
@@ -1,37 +1,48 @@
-import React, { Component } from 'react';
-import { Redirect, Link } from 'react-router-dom';
-import { Container, Row, Col } from '../../components/Grid';
-import { Card } from '../../components/Card';
-import { Input, FormBtn } from '../../components/Form';
-import AUTH from '../../utils/AUTH';
+import React, { Component, ChangeEvent } from "react";
+import { Redirect, Link } from "react-router-dom";
+import { Container, Row, Col } from "../../components/Grid";
+import { Card } from "../../components/Card";
+import { Input, FormBtn } from "../../components/Form";
+import AUTH from "../../utils/AUTH";
 
 import "./Auth.css";
 
-class SignupForm extends Component {
+interface Props {}
+interface State {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+  redirectTo: string | null;
+}
 
-	constructor() {
-    super();
-    
-		this.state = {
-      firstName: '',
-      lastName: '',
-			username: '',
-			password: '',
-			confirmPassword: '',
-			redirectTo: null
-		};
+class SignupForm extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+      redirectTo: null
+    };
   }
-  
-	handleChange = (event) => {
-		this.setState({
-			[event.target.name]: event.target.value
-		});
-  }
-  
-	handleSubmit = (event) => {
-		event.preventDefault();
-		// TODO - validate!
-		AUTH.signup({
+
+  handleChange = (event: ChangeEvent<{ name: string; value: unknown }>) => {
+    const { name } = event.target;
+    this.setState((prevState: State) => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // TODO - validate!
+    AUTH.signup({
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       username: this.state.username,
@@ -39,67 +50,67 @@ class SignupForm extends Component {
     }).then(response => {
       console.log(response);
       if (!response.data.error) {
-        console.log('youre good');
+        console.log("youre good");
         this.setState({
-          redirectTo: '/'
+          redirectTo: "/"
         });
       } else {
-        console.log('duplicate');
+        console.log("duplicate");
       }
     });
-  }
-  
-	render() {
-		if (this.state.redirectTo) {
-			return <Redirect to={{ pathname: this.state.redirectTo }} />
+  };
+
+  render() {
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />;
     }
-    
-		return (
+
+    return (
       <Container>
-            <Card title="Lets Play Yahtzee!">
-              <form style={{marginTop: 10}}>
-                <label htmlFor="username">First name: </label>
-                <Input
-                  type="text"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="username">Last name: </label>
-                <Input
-                  type="text"
-                  name="lastName"
-                  value={this.state.lastName}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="username">Username: </label>
-                <Input
-                  type="text"
-                  name="username"
-                  value={this.state.username}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="password">Password: </label>
-                <Input
-                  type="password"
-                  name="password"
-                  value={this.state.password}
-                  onChange={this.handleChange}
-                />
-                <label htmlFor="confirmPassword">Confirm Password: </label>
-                <Input
-                  type="password"
-                  name="confirmPassword"
-                  value={this.state.confirmPassword}
-                  onChange={this.handleChange}
-                />
-                <Link to="/">Login</Link>
-                <FormBtn onClick={this.handleSubmit}>Register</FormBtn>
-              </form>
-            </Card>
+        <Card title="Lets Play Yahtzee!">
+          <form style={{ marginTop: 10 }}>
+            <label htmlFor="username">First name: </label>
+            <Input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="username">Last name: </label>
+            <Input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="username">Username: </label>
+            <Input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="password">Password: </label>
+            <Input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="confirmPassword">Confirm Password: </label>
+            <Input
+              type="password"
+              name="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+            />
+            <Link to="/">Login</Link>
+            <FormBtn onClick={this.handleSubmit}>Register</FormBtn>
+          </form>
+        </Card>
       </Container>
-		)
-	}
+    );
+  }
 }
 
 export default SignupForm;
