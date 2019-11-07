944d9d257a665d5d780a6758cff6f57d331845af
ocskier <ocskier@gmail.com>
2019-11-07 09:01:17 -0500

    adjusted card styling

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -6,10 +6,12 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = props => (
-  <div className="card">
+  <div className="card" style={{ maxWidth: '650px' }}>
     <div className="card-header bg-primary" style={{ color: '#fff' }}>
       <h5>{props.title}</h5>
     </div>
-    <div className="card-body">{props.children}</div>
+    <div className="card-body" style={{ margin: 'auto' }}>
+      {props.children}
+    </div>
   </div>
 );
